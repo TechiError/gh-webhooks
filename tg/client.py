6480664cf071d9b51c20111fea406d6e272b3fde
Error <error@notavailable.live>
2021-04-25 18:38:04 +0530

    Update client.py

--- tg/client.py
+++ tg/client.py
@@ -1,9 +1,6 @@
 from decouple import config
 #from telethon import Button, TelegramClient, events
-from pyrogram import (
-    Client,
-    __version__
-)
+from telethon import TelegramClient
 import logging
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
@@ -13,4 +10,6 @@ APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 
-tgbot = Client(session_name="kensur", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+tgbot = TelegramClient("kensur", api_id=APP_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
+
+tgbot.run_until_disconnected()
