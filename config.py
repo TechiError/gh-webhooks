be83508642293ad5c7008d298a6cd7a193b263a2
Caleb Noble <uniqisuniq@outlook.com>
2022-02-10 10:37:54 +0530

    Aren't these useless?

--- config.py
+++ config.py
@@ -6,10 +6,8 @@ from telethon import TelegramClient
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
 )
-APP_ID = config("API_ID")
-API_HASH = config("API_HASH")
 BOT_TOKEN = config("TOKEN")
 AUTH_CHATS = config("AUTH_CHATS")
-tgbot = TelegramClient(None, APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
+tgbot = TelegramClient(None, 6, "eb06d4abfb49dc3eeb1aeb98ae0f581e").start(bot_token=BOT_TOKEN)
 
 print("OK?")
